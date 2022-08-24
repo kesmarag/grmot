76806ac0079281751397b582be540b329448ee70
Costas Smaragdakis <kesmarag@gmail.com>
2022-08-24 13:51:29 +0300

    update

--- examples/start_jupyter.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/home/kesmarag/.venvs/env3/bin/jupyter notebook --ip='0.0.0.0' --port=8888 --no-browser --NotebookApp.token='seismic' --notebook-dir="/home/kesmarag/Git/grmot/examples"  
